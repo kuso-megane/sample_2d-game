04939375a9f88bb861baab87ee2bb0b3fc491521
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-07-26 12:17:37 +0900

    playerController.cs 左右の移動を可能に

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rigidBody;
+    private float axisH;
+    public float speed;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.rigidBody = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        this.axisH = Input.GetAxisRaw("Horizontal");
+    }
+
+
+    private void FixedUpdate() {
+        rigidBody.velocity = new Vector2(axisH * speed, rigidBody.velocity.y);
     }
 }
